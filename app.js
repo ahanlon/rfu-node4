e5cd537e906242043407ab7d8ad330162a1b42f5
ahanlon <hanlonad27@gmail.com>
2015-07-22 16:16:54 -0600

    first commit basic functionality

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var indexController = require('./controllers/index.js');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', indexController.index);
+
+app.get('/canary-islands', function(req, res){
+	res.render('canary-islands');
+});
+
+app.get('/cape-verde', function(req, res){
+	res.render('cape-verde');
+});
+
+app.get('/strait-of-magellan', function(req, res){
+	res.render('strait-of-magellan');
+});
+
+app.get('/guam', function(req, res){
+	res.render('guam');
+});
+
+app.get('/phillipines', function(req, res){
+	res.render('phillipines');
+});
+
+var server = app.listen(8431, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
